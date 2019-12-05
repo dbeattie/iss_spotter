5a30254671cd0f804216ea2f119499ef824421b1
dbeattie <darren@fettle.ca>
2019-12-05 18:50:14 +0000

    created a function to get ISS passover times from an API and return them as stringified JSON file

--- index3.js
+++ index3.js
@@ -9,24 +9,25 @@
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-const { fetchCoordsByIP } = require('./iss1');
+// const { fetchCoordsByIP } = require('./iss1');
 
-fetchCoordsByIP(`66.207.199.230`, (error, data) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-
-  console.log('It worked! Returned Coords:' , data);
-});
-
-
-
-// fetchCoordsByIP('162.245.144.188', (error, coords) => {
+// fetchCoordsByIP(`66.207.199.230`, (error, coordsData) => {
 //   if (error) {
 //     console.log("It didn't work!" , error);
 //     return;
 //   }
 
-//   console.log('It worked! Returned Coords:' , coords);
+//   console.log('It worked! Returned Coords:' , coordsData);
 // });
+
+const { fetchISSFlyOverTimes } = require('./iss1');
+const exampleCoords = { latitude: '43.6', longitude: '-79.4' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned passover times:' , passTimes);
+});
--- iss1.js
+++ iss1.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = "https://api.ipify.org?format=json";
 
 //GETS COMPUTER'S IP ADDDRESS
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   request(url, (error, response, body) => {
     if (error) {
       return callback(error, null);
@@ -13,25 +13,45 @@ const fetchMyIP = function(callback) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       return callback(Error(msg), null);
     }
+
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
 };
 
 //GETS COMPUTER'S LOCATION COORDINATES BASED ON THE IP ADDRESS
-const fetchCoordsByIP = function(ip, callback) { 
-  request (`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
     if (error) {
-      return callback(error, null)
+      return callback(error, null);
     }
 
     if (response.statusCode !== 200) {
-      const msg1 = `Status code ${response.statusCode} when fetching coordinates of IP. Response: ${body}`
-      return callbackk(Error(msg1), null);
+      const msg1 = `Status code ${response.statusCode} when fetching coordinates of IP. Response: ${body}`;
+      return callback(Error(msg1), null);
     }
+
     const { latitude, longitude } = JSON.parse(body).data;
     callback(null, { latitude, longitude});
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url1 = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+  request(url1, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg2 = `Status code ${response.statusCode} when fetching coordinates of ISS PassTimes. Response: ${body}`;
+      return callback(Error(msg2), null);
+    }
+
+    const passTimes = JSON.parse(body).response;
+    callback(null, passTimes);
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
